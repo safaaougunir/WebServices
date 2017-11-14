62de624beee3fc8ce32dea5e4e308ed62ebc0c4e
safaaoug <safaa.ougunir@um5s.net.ma>
2017-11-14 23:03:30 +0100

    Merge branch 'feature_ListWithOnLight'

--- src/main/java/com/test/demo/DAO/RoomDaoCustom.java
+++ src/main/java/com/test/demo/DAO/RoomDaoCustom.java
@@ -1,9 +1,11 @@
 package com.test.demo.DAO;
 
+import com.test.demo.Models.Light;
 import com.test.demo.Models.Room;
 
 import javax.persistence.TypedQuery;
+import java.util.List;
 
-public interface RoomDaoCustom {
-    public TypedQuery<Room> switchLight(long id);
+public interface RoomDaoCustom  {
+    public List<Room> findRoomsWithOnLight();
 }
